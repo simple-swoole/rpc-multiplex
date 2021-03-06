b2ec02fb082f90da59bf4a24c2f8d14898e691d5
李铭昕 <715557344@qq.com>
2021-03-06 09:16:01 +0800

    Fixed PHPStan

--- src/CallWithConsul.php
+++ src/CallWithConsul.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 /**
  * This file is part of Simps.
  *
@@ -8,18 +8,38 @@ declare(strict_types = 1);
  * @document https://doc.simps.io
  * @license  https://github.com/simple-swoole/simps/blob/master/LICENSE
  */
-
 namespace Simps\RpcMultiplex;
 
+use GuzzleHttp\Client as guzzle;
 use Multiplex\Socket;
 use Simps\Consul\Health;
-use GuzzleHttp\Client as guzzle;
 
 class CallWithConsul extends Socket\Client
 {
-
     protected static $clientContainer = [];
-    
+
+    public function __construct(string $serverName, string $consulUri)
+    {
+        $health = new Health(function () use ($consulUri) {
+            return new guzzle(
+                [
+                    'base_uri' => $consulUri,
+                ]
+            );
+        });
+        $services = $health->service($serverName, ['passing' => true])->json();
+        if (empty($services)) {
+            throw new \Exception("no service '{$serverName}' is available");
+        }
+
+        //随机找一个
+        shuffle($services);
+        $point = current($services);
+        $ip = $point['Service']['Address'];
+        $port = $point['Service']['Port'];
+        parent::__construct($ip, (int) $port);
+    }
+
     public static function getInstance(string $serverName, string $consulUri)
     {
         if (empty(self::$clientContainer[$serverName])) {
@@ -32,34 +52,10 @@ class CallWithConsul extends Socket\Client
         return self::$clientContainer[$serverName];
     }
 
-    public function __construct(string $serverName, string $consulUri)
-    {
-
-        $health = new Health(function () use($consulUri) {
-            return new guzzle(
-                    [
-                'base_uri' => $consulUri
-                    ]
-            );
-        });
-        $services = $health->service($serverName, ['passing'=>true])->json();
-        if (empty($services)) {
-            throw new \Exception("no service '$serverName' is available");
-        }
-
-        //随机找一个
-        shuffle($services);
-        $point = current($services);
-        $ip = $point['Service']['Address'];
-        $port = $point['Service']['Port'];
-        parent::__construct($ip, (int) $port);
-    }
-
     public function call(string $class, string $method, ...$params): Protocol
     {
         $protocol = new Protocol($class, $method, $params);
 
         return unserialize(parent::request(serialize($protocol)));
     }
-
 }
--- src/TcpServer.php
+++ src/TcpServer.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 /**
  * This file is part of Simps.
  *
@@ -8,23 +8,21 @@ declare(strict_types = 1);
  * @document https://doc.simps.io
  * @license  https://github.com/simple-swoole/simps/blob/master/LICENSE
  */
-
 namespace Simps\RpcMultiplex;
 
+use GuzzleHttp\Client;
 use Multiplex\Packer;
 use Multiplex\Packet;
 use Multiplex\Serializer\StringSerializer;
 use Simps\Application;
+use Simps\Consul\Agent;
 use Simps\Listener;
 use Simps\Route;
 use Swoole\Coroutine;
 use Swoole\Server;
-use Simps\Consul\Agent;
-use GuzzleHttp\Client;
 
 class TcpServer
 {
-
     public static $incr = 0;
 
     /**
@@ -52,64 +50,19 @@ class TcpServer
      */
     protected static $serializer;
 
-
     public static function onStart(Server $server)
     {
-        $subs = config("servers.main.sub");
-        
+        $subs = config('servers.main.sub');
+
         foreach ($subs as $sub) {
             $ip = $sub['ip'];
             $port = $sub['port'];
             Application::echoSuccess("Swoole Multiplex RPC Server running：tcp://{$ip}:{$port}");
         }
-        
+
         self::registerService();
-        
+
         Listener::getInstance()->listen('start', $server);
-    }
-    
-
-    private static function registerService()
-    {
-        $subs = config("servers.main.sub");
-        foreach ($subs as $sub) {
-            if (isset($sub['consul'])) {//注册到consul
-                $agent = new Agent(function () use($sub) {
-                    return new Client(
-                            [
-                        'base_uri' => $sub['consul']['publish_to']
-                            ]
-                    );
-                });
-                if ($sub['ip'] == '0.0.0.0') {
-                    $ips = swoole_get_local_ip();
-                    if (empty($ips)) {
-                        Application::echoError("Service Register Fail, Cannot Get Ips");
-                        return;
-                    }
-                    $sub['ip'] = array_shift($ips);
-                }
-
-                foreach ($sub['service_name'] as $serverName) {
-                    try {
-                        $res = $agent->registerService([
-                                    'id' => $serverName,
-                                    'name' => $serverName,
-                                    'tags' => ["primary"],
-                                    'address' => $sub['ip'],
-                                    'port' => $sub['port'],
-                                    'meta' => [
-                                        'meta' => $serverName
-                                    ],
-                                    'checks' => $sub['consul']['checks'] ?? [],
-                                ])->getBody();
-                        Application::echoSuccess("Service {$serverName} Register to Consul Success");
-                    } catch (\Exception $exc) {
-                        Application::echoError("Service {$serverName} Register Fail");
-                    }
-                }
-            }
-        }
     }
 
     public static function onWorkerStart(Server $server, int $workerId)
@@ -141,4 +94,46 @@ class TcpServer
         });
     }
 
+    private static function registerService()
+    {
+        $subs = config('servers.main.sub');
+        foreach ($subs as $sub) {
+            if (isset($sub['consul'])) {//注册到consul
+                $agent = new Agent(function () use ($sub) {
+                    return new Client(
+                        [
+                            'base_uri' => $sub['consul']['publish_to'],
+                        ]
+                    );
+                });
+                if ($sub['ip'] == '0.0.0.0') {
+                    $ips = swoole_get_local_ip();
+                    if (empty($ips)) {
+                        Application::echoError('Service Register Fail, Cannot Get Ips');
+                        return;
+                    }
+                    $sub['ip'] = array_shift($ips);
+                }
+
+                foreach ($sub['service_name'] as $serverName) {
+                    try {
+                        $res = $agent->registerService([
+                            'id' => $serverName,
+                            'name' => $serverName,
+                            'tags' => ['primary'],
+                            'address' => $sub['ip'],
+                            'port' => $sub['port'],
+                            'meta' => [
+                                'meta' => $serverName,
+                            ],
+                            'checks' => $sub['consul']['checks'] ?? [],
+                        ])->getBody();
+                        Application::echoSuccess("Service {$serverName} Register to Consul Success");
+                    } catch (\Exception $exc) {
+                        Application::echoError("Service {$serverName} Register Fail");
+                    }
+                }
+            }
+        }
+    }
 }
